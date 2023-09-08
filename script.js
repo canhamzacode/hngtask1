b4e534a56141f1f654ebe48f101cc7c362ce290b
canhamzacode <abdulmuizzayo6@gmail.com>
2023-09-08 19:05:59 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// script.js
+const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+const currentDayOfTheWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+
+function updateUTCTimeAndDay() {
+    const currentTime = new Date();
+
+    // Update the UTC time
+    currentUTCTimeElement.textContent = currentTime.toUTCString();
+
+    // Get the current day of the week
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const dayOfWeek = currentTime.getDay();
+    const currentDayOfWeek = daysOfWeek[dayOfWeek];
+
+    // Update the current day of the week
+    currentDayOfTheWeekElement.textContent = currentDayOfWeek;
+}
+
+// Update the UTC time and day every second
+setInterval(updateUTCTimeAndDay, 1000);
+
+// Initialize the time and day
+updateUTCTimeAndDay();
